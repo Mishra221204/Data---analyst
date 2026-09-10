4c76c54c4599c2b346dc149e2a53d4441916c56b
Shubham Mishra <mishrashubham76395@gmail.com>
2026-09-10 12:00:07 +0530

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,39 @@
+#  str type in python
+
+# a='Welcome to back'
+# b="That is to all"
+# c='''Welcome to  "Jungla  that " all student '''
+
+# print(a)
+# print(b)
+# print(c)
+
+
+#  Slici in python
+
+# name="shubham Mishra"
+
+# print(name[8])  # slice [stop]
+# print(name[0:6])    # slic[start: stop]
+# print(name[0:8:2])   # slic[start: stop:step] 
+
+
+#  reverse
+# teacher= "MaM"
+# print(teacher[::-1])   # reverse
+
+# value='My name is Shubham'
+# print(value[::-1])           # reverse
+
+
+
+#  type casting in python
+# int(), float(),double(),str()
+
+#  1. int()
+
+a=12.34
+b=int(a)
+print(b)
+print(type(b))   # check data types
+          
